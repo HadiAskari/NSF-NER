fb1a3be68a971454599bf819df61c46186c4b297
Hadi Askari <haskari@ucdavis.edu>
2024-10-27 19:41:55 -0700

    Delete RE directory

--- RE/run.sh
+++
@@ -1,5 +0,0 @@
-python -m script_llama3_hacker \
---text "The Microsoft Exchange Server has been a frequent target of cyber espionage campaigns, with zero-day vulnerabilities allowing attackers to compromise sensitive communication systems. Similarly, Fortinet FortiOS, the operating system for Fortinet security appliances, has faced vulnerabilities exploited by threat actors to bypass network protections, compromising connected hardware. These software and hardware platforms underscore the ongoing risks posed by unpatched vulnerabilities within critical enterprise environments." \
---software '["Microsoft Exchange Server", "Fortinet FortiOS"]' \
---hardware '["Fortinet security appliances"]' \
---vulnerability '["Zero-day vulnerabilities", "Unpatched vulnerabilities"]'
